f345e56ffdd5c8ed6a48cbdf288b09df159cae28
dibo <sh.eldeeb.2010@gmail.com>
2022-04-16 09:18:38 +0000

    feat: release.config addReleases

--- release.config.js
+++ release.config.js
@@ -2,7 +2,7 @@ module.exports = {
   branches: ["release-github"],
   plugins: [
     "@semantic-release/commit-analyzer",
-    ["@semantic-release/github", { assets: ["dist"] }],
+    ["@semantic-release/github", { assets: ["dist"], addReleases: "top" }],
   ],
   // extends: ["semantic-release-monorepo"],
 };
