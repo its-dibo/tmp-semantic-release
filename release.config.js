d105fd2924ffc77336d32d93b9faa9166a3a7869
dibo <sh.eldeeb.2010@gmail.com>
2022-04-16 15:03:25 +0000

    fix: releas.config.github

--- release.config.js
+++ release.config.js
@@ -2,7 +2,7 @@ module.exports = {
   branches: ["release-github"],
   plugins: [
     "@semantic-release/commit-analyzer",
-    ["@semantic-release/github", { assets: ["dist"], addReleases: "top" }],
+    ["@semantic-release/github", { assets: [["dist"]], addReleases: "top" }],
   ],
   // extends: ["semantic-release-monorepo"],
 };
