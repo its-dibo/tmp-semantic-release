2a69577684593f4d266b7771952584c8c53313a6
dibo <sh.eldeeb.2010@gmail.com>
2022-04-16 09:13:44 +0000

    feat: release.config assets

--- release.config.js
+++ release.config.js
@@ -2,7 +2,10 @@ module.exports = {
   branches: ["release-github"],
   plugins: [
     "@semantic-release/commit-analyzer",
-    ["@semantic-release/github", { assets: ["dist/**/*.*"] }],
+    [
+      "@semantic-release/github",
+      { assets: [{ path: "dist/**/*.*", label: "dist assets" }] },
+    ],
   ],
   // extends: ["semantic-release-monorepo"],
 };
