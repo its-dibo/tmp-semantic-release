18782d4e59e81aa96db11e04fc461be931d3a608
Dibo <sh.eldeeb.2010@gmail.com>
2022-05-09 12:58:57 +0000

    build: release.config

--- package/release.config.js
+++ package/release.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     ["@semantic-release/npm", { pkgRoot: "dist" }],
+    "@semantic-release/git",
   ],
   extends: ["semantic-release-monorepo"],
 };
