915f0814f2a1a7179afbba88c971a0d21c5fff43
Dibo <sh.eldeeb.2010@gmail.com>
2022-05-13 00:13:04 +0000

    build: release.config: add git

--- package/release.config.js
+++ package/release.config.js
@@ -2,6 +2,12 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     ["@semantic-release/npm", { pkgRoot: "dist" }],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "npm-shrinkwrap.json"],
+      },
+    ],
   ],
   extends: ["semantic-release-monorepo"],
 };
